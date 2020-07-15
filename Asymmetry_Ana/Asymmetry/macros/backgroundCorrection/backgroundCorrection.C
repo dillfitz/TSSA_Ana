2cd86aca0513fe195ecd1b7207888e580c4203b9
Dillon Fitzgerald <dillfitz@qcdc2.physics.lsa.umich.edu>
2020-07-15 02:00:23 -0400

    Added background correction calculation macro

---
+++ Asymmetry_Ana/Asymmetry/macros/backgroundCorrection/backgroundCorrection.C
@@ -0,0 +1,153 @@
+#include <cmath>
+#include "../../Constants.h"
+
+void backgroundCorrection()
+{
+    int verbosity = 0;
+    bool jpsi = 1;
+    bool hadron = 0;
+    const int nbins = NUM_VALUE_BINS;
+    double x_lumi[nbins], y_lumi[nbins], x_sqrt[nbins], y_sqrt[nbins];
+    double y_corrected_lumi[nbins], y_corrected_sqrt[nbins];
+	double corrected_lumi_err[nbins], corrected_sqrt_err[nbins];
+    double total_bg[nbins], had_AN[nbins], had_AN_err[nbins];
+
+    // Taken from previous 200 GeV PHENIX measurements //
+    double jpsi_AN[nbins] = {-0.068, -0.068, -0.068, -0.068};
+    double jpsi_AN_err[nbins] = {0.188, 0.188, 0.188, 0.188};
+    double hadminus_AN[nbins] = {-0.0012, -0.0012, -0.021, -0.021};
+    double hadminus_AN_err[nbins] = {0.0082, 0.0082, 0.027, 0.027};
+    double hadplus_AN[nbins] = {-0.0054, -0.0054, -0.031, -0.031};
+    double hadplus_AN_err[nbins] = {0.0078, 0.0078, 0.026, 0.026};
+
+    //TGraphAsymmErrors *corrected_lumi = new TGraphAsymmErrors();
+
+    TFile *infile1 = TFile::Open("inputFiles/normalizations.root");
+    TFile *infile2 = TFile::Open("inputFiles/systematics.root");
+    TFile *infile3 = TFile::Open("inputFiles/hadcontam.root");
+    TFile *infile4 = TFile::Open("inputFiles/curated_ohfe.root");
+
+    TTree* eTree = (TTree*)infile4->Get( "e_svx_tree" );
+
+    TH1F* h_piz_n = (TH1F*)infile1->Get("h_piz_n");
+    TH1F* h_eta_n = (TH1F*)infile1->Get("h_eta_n");
+    TH1F* h_jpsi_n = (TH1F*)infile1->Get("h_jpsi_n");
+    TH1F* h_ke3_n = (TH1F*)infile1->Get("h_ke3_n");
+    TH1F* h_photon_n = (TH1F*)infile1->Get("h_photon_n");
+    TH1F* h_hadcontam_n = (TH1F*)infile1->Get("h_hadcontam_n");
+    TH1F* h_sys_piz = (TH1F*)infile2->Get("h_sys_piz");
+    TH1F* h_sys_eta = (TH1F*)infile2->Get("h_sys_eta");
+    TH1F* h_sys_ke3 = (TH1F*)infile2->Get("h_sys_ke3");
+    TH1F* h_sys_jpsi = (TH1F*)infile2->Get("h_sys_jpsi");
+    TH1F* h_sys_photon = (TH1F*)infile2->Get("h_sys_photon");
+    TH1F* h_sys_hadlow = (TH1F*)infile3->Get("h_depfit")->Clone("h_sys_hadlow");
+    TH1F* h_sys_hadhigh = (TH1F*)infile3->Get("h_algebraic")->Clone("h_sys_hadhigh");
+    TH1F* h_sys_hadnom = (TH1F*)infile3->Get("h_hadcontam")->Clone("h_sys_hadnom");
+    TH1I* h_charge1 = new TH1I("ch1", "", 4, -1, 2);
+    TH1I* h_charge2 = new TH1I("ch2", "", 4, -1, 2);
+    TH1I* h_charge3 = new TH1I("ch3", "", 4, -1, 2);
+    TH1I* h_charge4 = new TH1I("ch4", "", 4, -1, 2);
+
+    eTree->Draw("charge>>ch1","pt>1.5 && pt<1.8");
+    eTree->Draw("charge>>ch2","pt>1.8 && pt<2.1");
+    eTree->Draw("charge>>ch3","pt>2.1 && pt<2.7");
+    eTree->Draw("charge>>ch4","pt>2.7 && pt<5.0");
+
+    double minus_fraction[nbins] = 
+    { 
+        h_charge1->GetBinContent(1)/(h_charge1->GetBinContent(1) + h_charge1->GetBinContent(3)),
+        h_charge2->GetBinContent(1)/(h_charge2->GetBinContent(1) + h_charge2->GetBinContent(3)),
+        h_charge3->GetBinContent(1)/(h_charge3->GetBinContent(1) + h_charge3->GetBinContent(3)),
+        h_charge4->GetBinContent(1)/(h_charge4->GetBinContent(1) + h_charge4->GetBinContent(3)) 
+    };
+
+    TFile *infile0 = TFile::Open("inputFiles/ohfe_AN.root");
+
+    for (int i=0; i<nbins; ++i)
+    {
+
+        had_AN[i] = minus_fraction[i]*hadminus_AN[i] + (1-minus_fraction[i])*hadplus_AN[i];
+        had_AN_err[i] = minus_fraction[i]*hadminus_AN_err[i] + (1-minus_fraction[i])*hadplus_AN_err[i];
+		
+        lumi->GetPoint(i, x_lumi[i], y_lumi[i]);
+        sqrt0->GetPoint(i, x_sqrt[i], y_sqrt[i]);
+        if ( verbosity == 1 )
+        {
+            cout << "minus fraction : " << minus_fraction[i] << endl;
+            cout << "charged hadron AN : " << had_AN[i] << " err : " << had_AN_err[i] << endl;
+            cout << "lumi : " << y_lumi[i] << endl;
+        }
+	    //total_bg[i] = h_piz_n->GetBinContent(i+1) + h_eta_n->GetBinContent(i+1) + h_photon_n->GetBinContent(i+1) + h_jpsi_n->GetBinContent(i+1) + h_hadcontam_n->GetBinContent(i+1) + h_ke3_n->GetBinContent(i+1);
+	    total_bg[i] = h_piz_n->GetBinContent(i+1) + h_eta_n->GetBinContent(i+1) + h_photon_n->GetBinContent(i+1) + h_jpsi_n->GetBinContent(i+1) + h_hadcontam_n->GetBinContent(i+1);
+	    if ( hadron && jpsi )
+		{
+	        y_corrected_lumi[i] = (y_lumi[i] - h_jpsi_n->GetBinContent(i+1)*jpsi_AN[i] - h_hadcontam_n->GetBinContent(i+1)*had_AN[i])/(1-total_bg[i]);
+	        y_corrected_sqrt[i] = (y_sqrt[i] - h_jpsi_n->GetBinContent(i+1)*jpsi_AN[i] - h_hadcontam_n->GetBinContent(i+1)*had_AN[i])/(1-total_bg[i]);	
+			corrected_lumi_err[i] = std::sqrt(std::pow(lumi->GetErrorYhigh(i), 2) + std::pow(h_jpsi_n->GetBinContent(i+1)*jpsi_AN_err[i],2) + std::pow(h_hadcontam_n->GetBinContent(i+1)*had_AN_err[i],2))/(1-total_bg[i]);
+			corrected_sqrt_err[i] = std::sqrt(std::pow(sqrt0->GetErrorYhigh(i), 2) + std::pow(h_jpsi_n->GetBinContent(i+1)*jpsi_AN_err[i],2) + std::pow(h_hadcontam_n->GetBinContent(i+1)*had_AN_err[i],2))/(1-total_bg[i]);
+		}
+	    else if (hadron)
+		{
+	        y_corrected_lumi[i] = (y_lumi[i] - h_hadcontam_n->GetBinContent(i+1)*had_AN[i])/(1-total_bg[i]);
+	        y_corrected_sqrt[i] = (y_sqrt[i] - h_hadcontam_n->GetBinContent(i+1)*had_AN[i])/(1-total_bg[i]);
+			corrected_lumi_err[i] = std::sqrt(std::pow(lumi->GetErrorYhigh(i), 2) + std::pow(h_hadcontam_n->GetBinContent(i+1)*had_AN_err[i],2))/(1-total_bg[i]);
+			corrected_sqrt_err[i] = std::sqrt(std::pow(sqrt0->GetErrorYhigh(i), 2) +  std::pow(h_hadcontam_n->GetBinContent(i+1)*had_AN_err[i],2))/(1-total_bg[i]);
+		}	
+	    else if (jpsi)
+		{
+	        y_corrected_lumi[i] = (y_lumi[i] - h_jpsi_n->GetBinContent(i+1)*jpsi_AN[i])/(1-total_bg[i]);
+	        y_corrected_sqrt[i] = (y_sqrt[i] - h_jpsi_n->GetBinContent(i+1)*jpsi_AN[i])/(1-total_bg[i]);
+			corrected_lumi_err[i] = std::sqrt(std::pow(lumi->GetErrorYhigh(i), 2) + std::pow(h_jpsi_n->GetBinContent(i+1)*jpsi_AN_err[i],2))/(1-total_bg[i]);
+			corrected_sqrt_err[i] = std::sqrt(std::pow(sqrt0->GetErrorYhigh(i), 2) + std::pow(h_jpsi_n->GetBinContent(i+1)*jpsi_AN_err[i],2))/(1-total_bg[i]);
+		}	
+	    else
+		{
+            y_corrected_lumi[i] = (y_lumi[i])/(1-total_bg[i]);
+            y_corrected_sqrt[i] = (y_sqrt[i])/(1-total_bg[i]);
+			corrected_lumi_err[i] = lumi->GetErrorYhigh(i)/(1-total_bg[i]);
+			corrected_sqrt_err[i] = sqrt0->GetErrorYhigh(i)/(1-total_bg[i]);
+		}
+
+        if ( verbosity == 1 )	
+	        cout << "corrected lumi : " << y_corrected_lumi[i] << endl;
+
+        //corrected_lumi->SetPoint(x_lumi[i], y_corrected_lumi[i]);
+    } 
+ 
+	double ptLow[nbins];
+	double ptHigh[nbins];
+    for( int i = 0; i < NUM_VALUE_BINS; ++i )
+    {
+        ptLow[i]  = fabs( VALUE_BINS[i] - x_lumi[i] );
+        ptHigh[i] = fabs( VALUE_BINS[i + 1] - x_lumi[i] );
+    }
+	TGraphAsymmErrors *corrected_lumi = new TGraphAsymmErrors( nbins, x_lumi, y_corrected_lumi, ptLow, ptHigh, corrected_lumi_err, corrected_lumi_err); 
+	TGraphAsymmErrors *corrected_sqrt = new TGraphAsymmErrors( nbins, x_sqrt, y_corrected_sqrt, ptLow, ptHigh, corrected_sqrt_err, corrected_sqrt_err); 
+
+	if (hadron && jpsi )
+		corrected_sqrt->SetTitle("Background Corrected A_{N}");
+	else if (hadron)
+		corrected_sqrt->SetTitle("Background Corrected A_{N} with A_{N}^{J/#psi} = 0 ");
+	else if (jpsi)
+		corrected_sqrt->SetTitle("Background Corrected A_{N} with A_{N}^{h^{#pm}} = 0 ");
+	else
+		corrected_sqrt->SetTitle("Background Corrected A_{N} with A_{N}^{J/#psi} = A_{N}^{h^{#pm}} = 0 ");
+
+    TLegend *legend = new TLegend( 0.55, 0.9, 0.95, 0.7 );
+    legend->AddEntry( corrected_sqrt, "Square Root Formula", "lep" );
+    legend->AddEntry( corrected_lumi, "Relative Luminosity Formula", "lep" );
+
+    corrected_lumi->SetMarkerStyle( kFullDiamond );
+    corrected_lumi->SetMarkerColor( kViolet - 6 );
+    corrected_lumi->SetMarkerSize( 2 );
+
+    corrected_sqrt->SetMarkerStyle( kFullSquare );
+    corrected_sqrt->SetMarkerColor( kRed );
+    corrected_sqrt->SetMarkerSize( 0.7* 2 );
+
+	corrected_sqrt->Draw("AP");
+	corrected_lumi->Draw("P");
+	legend->Draw();
+	//corrected_sqrt->Draw("SAME");
+
+}
